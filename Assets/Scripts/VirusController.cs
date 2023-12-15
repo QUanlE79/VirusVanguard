e79bfe83e177e8d0f0ada6295e6c7325132dc22d
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2023-12-15 17:15:09 +0700

    Varus move with input system

--- Assets/Scripts/VirusController.cs
+++ Assets/Scripts/VirusController.cs
@@ -1,43 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class VirusController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     float horizontal;
     float vertical;
-    public float speed = 2.5f;
+    public float speed = 5f;
      Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
+    Vector2 moveInput;
+    [SerializeField]
+    private bool _isMoving=false;
+    public bool IsMoving { get {
+            return _isMoving;
+        } 
+        private set { 
+            _isMoving= value;
+            animator.SetBool("isMoving", value);
+        } 
+    }
+
     // Start is called before the first frame update
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
     void Start()
     {
-         rb2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+         
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        Vector2 move = new Vector2(horizontal, vertical);
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(0.0f,move.y))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
+        
+        
     }
     private void FixedUpdate()
     {
-        Vector2 position = transform.position;
-        position.x += (speed * horizontal * Time.deltaTime);
-        position.y += (speed * vertical * Time.deltaTime);
-        rb2d.MovePosition(position);
-    }
+        rb2d.velocity = new Vector2(moveInput.x*speed, rb2d.velocity.y);
+        if (!Mathf.Approximately(moveInput.x, 0.0f) || !Mathf.Approximately(0.0f, moveInput.y))
+        {
+            lookDirection.Set(moveInput.x, moveInput.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("LookX", lookDirection.x);
 
+    }
+    public void onMove(InputAction.CallbackContext context)
+    {
+        moveInput=context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+    }
 }
