89f3fad5cf955446204e63f24fdf4fe8d57c2588
PhuocNhu <nguyennhuphuoc2002@gmail.com>
2023-12-26 17:26:41 +0700

    add BossStagePhuoc and add deadzone

--- Assets/Scripts/DeadZoneController.cs
+++ Assets/Scripts/DeadZoneController.cs
@@ -8,17 +8,11 @@ public class SpikeDamage : MonoBehaviour
     public int damage = 10;
 
     private void OnTriggerStay2D(Collider2D collision) {
-        // VirusController controller = other.GetComponent<VirusController>();
-        // if(controller != null){
-        //     controller.onHit(1,Vector2.zero);
-        // }
-        // Debug.Log(controller.name);
-        Damageable damageable= collision.GetComponent<Damageable>();
+       
+        PlayerDamageable damageable= collision.GetComponent<PlayerDamageable>();
         if (damageable != null)
         {
-
             damageable.Hit(damage, knockback);
-            Debug.Log(collision.name + "hit for " + damage);
         }
     }
 
