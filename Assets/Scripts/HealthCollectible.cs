5eb44fecb311f12bb04aafbcb5196d70066e1dea
tonthatbach2002 <tonthatbach2002@gmail.com>
2023-12-28 16:34:13 +0700

    Add coin and collectilbe sounds

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -6,6 +6,7 @@ public class HealthCollectible : MonoBehaviour
 {
     public int healAmount = 20;
     public Vector3 spinRotationSpeed= new Vector3(0,180,0);
+    public AudioSource pickUp;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +17,19 @@ public class HealthCollectible : MonoBehaviour
     void Update()
     {
         transform.eulerAngles += spinRotationSpeed * Time.deltaTime;
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerDamageable damageable= collision.GetComponent<PlayerDamageable>();
+        
         if (damageable)
         {
+            
             bool washealed=damageable.Health(healAmount);
             if (washealed)
             {
+                pickUp.Play();
                 Destroy(gameObject);
             }
         }
